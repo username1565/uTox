66c6faa3d8e8067450ebf6a0ce3e5739f10996b7
username1565 <39200328+username1565@users.noreply.github.com>
2020-04-22 06:32:34 +0300

    Fix error of crushing uTox on start in Windows XP.
    Resolve issue https://github.com/username1565/uTox_winXP/issues/4
    by updating libraries of "libsodium" and "libtoxcore".
    
    1. The minimal changes to prevent that bug:
    will be enough to update the file "libsodium.a" only,
    from new library: "libsodium-1.0.18_build_windows_x86.zip",
    because Jenkins compilator was been too old, to make the correct building this library automatically.
    
    2. But in this commit, the source code of libsodium updated too, up to v1.0.18.
    libtoxcore updated to lite-version, without libsodium, and another garbage, inside.
    Now, on extract "libtoxcore", "libsodium"-files not will be overwritted.
    
    3. Also, was been changed the links to download files in "get_libs_and_unzip_it.sh",
    was been commented old files, but leaved this, and made the runing extract "libsodium", after extract all another libraries,
    if old version of "libtoxcore" will be used for testing compilation,
    or for comparison the changes and differences between source codes of this libraries.

--- libs/windows-x32/get_libs_and_unzip_it.sh
+++ libs/windows-x32/get_libs_and_unzip_it.sh
@@ -4,24 +4,41 @@ apt-cyg install bsdtar	#install bsd-tar archiver to zip-unazip this zip-files.
 echo "Done."
 sleep 1
 
+
+
 #	download packages there, if this does not exists
 echo "Download libraries zip-files, if does not exists"
 wget --no-clobber https://build.tox.chat/view/libtoxcore/job/libtoxcore-toktok_build_windows_x86_static_release/lastSuccessfulBuild/artifact/libtoxcore-toktok_build_windows_x86_static_release.zip
 wget --no-clobber https://build.tox.chat/view/libopenal/job/libopenal_build_windows_x86_static_release/lastSuccessfulBuild/artifact/libopenal_build_windows_x86_static_release.zip
-wget --no-clobber https://build.tox.chat/view/libsodium/job/libsodium_build_windows_x86_static_release/lastSuccessfulBuild/artifact/libsodium_build_windows_x86_static_release.zip
+
+#invalid compiler in this version
+#wget --no-clobber https://build.tox.chat/view/libsodium/job/libsodium_build_windows_x86_static_release/lastSuccessfulBuild/artifact/libsodium_build_windows_x86_static_release.zip
+
+#fixed
+wget --no-clobber https://github.com/blueclouds8666/uTox_XP/raw/files/libraries-precompiled/windows-x86/libsodium-1.0.18_build_windows_x86.zip
+
 wget --no-clobber https://build.tox.chat/view/libvpx/job/libvpx_build_windows_x86_static_release/lastSuccessfulBuild/artifact/libvpx_build_windows_x86_static_release.zip
 wget --no-clobber https://build.tox.chat/view/libopus/job/libopus_build_windows_x86_static_release/lastSuccessfulBuild/artifact/libopus_build_windows_x86_static_release.zip
 wget --no-clobber https://build.tox.chat/view/libfilteraudio/job/libfilteraudio_build_windows_x86_static_release/lastSuccessfulBuild/artifact/libfilteraudio.zip
 echo "Done."
 sleep 1
 
+
+
 echo "Unzip zip-files, using bsdtar archiver";
 #	unzip it in this filder by this sequence:
 bsdtar xf libtoxcore-toktok_build_windows_x86_static_release.zip
 bsdtar xf libopenal_build_windows_x86_static_release.zip
-bsdtar xf libsodium_build_windows_x86_static_release.zip
+
+#do not extract this.
+#bsdtar xf libsodium_build_windows_x86_static_release.zip
+
 bsdtar xf libvpx_build_windows_x86_static_release.zip
 bsdtar xf libopus_build_windows_x86_static_release.zip
 bsdtar xf libfilteraudio.zip
+
+#extract this in the end and overwrite the files
+bsdtar xf libsodium-1.0.18_build_windows_x86.zip
+
 echo "Done.";
 sleep 1
